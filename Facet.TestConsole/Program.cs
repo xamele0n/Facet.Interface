314d1f43e086cc775b573d880be49435a28862ad
Tim Maes <maestim@cronos.be>
2025-04-22 21:31:42 +0200

    Add project files.

---
+++ Facet.TestConsole/Program.cs
@@ -0,0 +1,53 @@
+﻿using Facet;
+
+namespace Redacto.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Redacted props:");
+            foreach (var prop in typeof(PersonWithoutEmail).GetProperties())
+            {
+                Console.WriteLine($"- {prop.Name}");
+            }
+
+            Console.WriteLine("Redacted props only name:");
+            foreach (var prop in typeof(PersonWithOnlyName).GetProperties())
+            {
+                Console.WriteLine($"- {prop.Name}");
+            }
+
+            Console.WriteLine("Redacted props extra property:");
+            foreach (var prop in typeof(PersonWithOnlyNameAndExtraProperty).GetProperties())
+            {
+                Console.WriteLine($"- {prop.Name}");
+            }
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+    }
+
+    [Facet(typeof(Person), nameof(Person.Email))]
+    public partial class PersonWithoutEmail
+    {
+    }
+
+    [Facet(typeof(Person), new[] { nameof(Person.Email), nameof(Person.Age) })]
+    public partial class PersonWithOnlyName
+    {
+    }
+
+    [Facet(typeof(Person), new[] { nameof(Person.Email), nameof(Person.Age) })]
+    public partial class PersonWithOnlyNameAndExtraProperty
+    {
+        public string ExtraProperty { get; set; }
+    }
+}
---
+++ Facet/FacetAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Facet;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public sealed class FacetAttribute : Attribute
+{
+    public Type SourceType { get; }
+    public string[] Exclude { get; }
+
+    public FacetAttribute(Type sourceType, params string[] exclude)
+    {
+        SourceType = sourceType;
+        Exclude = exclude ?? Array.Empty<string>();
+    }
+}
